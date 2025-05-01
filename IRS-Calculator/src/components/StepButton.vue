112f123d99a9b7d2bdaeee5a25e6e8f10286ff92
Rui Dias <rui.dias@weezie.io>
2025-05-01 09:24:58 +0100

    refactor(Reusable components): Making the components reusable

---
+++ IRS-Calculator/src/components/StepButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button :class="[
+        'w-full px-6 py-2 font-semibold border-2 rounded-md cursor-pointer transition-colors duration-200',
+        disabled
+            ? 'text-[var(--primary-color)] border-[var(--primary-color)] opacity-50 cursor-not-allowed'
+            : 'bg-[var(--primary-color)] text-white border-[var(--primary-color)] hover:opacity-90'
+    ]" :disabled="disabled" @click="$emit('click')">
+        {{ label }}
+    </button>
+</template>
+
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: true
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+});
+</script>
---
+++ IRS-Calculator/src/components/WToolTip.vue
@@ -0,0 +1,25 @@
+<template>
+  <i
+    v-if="toolTip"
+    class="text-[var(--primary-color)] pi pi-info-circle"
+    v-tooltip.right="{
+      value: toolTip,
+      pt: {
+        text: 'text-xs',
+      },
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+    toolTip: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<style scoped></style>
